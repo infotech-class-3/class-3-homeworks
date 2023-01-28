2cb22145e2e619f4d85acc12321083fee4ae1509
ayildirim34 <ayildirim3457@protonmail.com>
2023-01-29 00:29:10 +0100

    Ahmet js-1

---
+++ 2023-01-22/01-Ahmet/odev5.js
@@ -0,0 +1,38 @@
+let x = 'Josefine';
+console.log(x);
+
+let vorname = 'Franz';
+
+let blume = 'Rose';
+let tier = 'Käfer';
+
+function hallo() {
+    return 'Hallo Welt!';
+    }
+console.log(hallo());
+
+function a() {
+    return 'Hallo a!';
+  }
+  function b() {
+    return 'Hallo b!';
+  }
+
+  console.log(a());
+  console.log(b());
+
+  function begruesse() {
+    return 'Moin!';
+  }
+  let begruessung= begruesse();
+  console.log(begruessung);
+
+  function echo(ton) {
+    return "ton";
+  }
+  console.log(echo());
+
+  function ping(ping) {
+    return "ping";
+  }
+  console.log(ping());
