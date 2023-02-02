04544167ebe24f13266464be246dcf4b2da5d05f
alipek-lab <alipek@gmail.com>
2023-02-02 12:28:34 +0100

    02-Ali JScript Homework
    The specified corrections have been made.

--- 2023-01-29/02-Ali/odev_2901.js
+++ 2023-01-29/02-Ali/odev_2901.js
@@ -1,33 +1,41 @@
 
-function hesap(sayi1,sayi2,islem){ // Fonksiyon tanimlandi
-    if (isNaN(sayi1)) { // Fonksiyon tanimlandi sayi1 degeri rakam kontrolu yapildi
-     hesapla.hata = 'Girilen değerlerden birincisi sayı değil! Hesaplama yapılamadı'; // sayi1 rakam degilse hata degeri yazildi
-     return; // islemin ilerlemesi durduruldu
+function hesap(sayi1,sayi2,islem){
+    
+   if (isNaN(sayi1)) {
+     hesapla.hata = 'Girilen değerlerden birincisi sayı değil! Hesaplama yapılamadı';
+     return;
     } 
-    else if(isNaN(sayi2)){ // Fonksiyon tanimlandi sayi2 degeri rakam kontrolu yapildi
-  hesapla.hata = 'Girilen değerlerden ikincisi sayı değil! Hesaplama yapılamadı'; // sayi2 rakam degilse hata degeri yazildi
-  return ;   // islemin ilerlemesi durduruldu
+    else if(isNaN(sayi2)){
+  hesapla.hata = 'Girilen değerlerden ikincisi sayı değil! Hesaplama yapılamadı';
+  return ;
     }
-    else if (islem == "bolme" && sayi2 === 0){ // islem bolme ve sayi2 0 olmasi kontrol edildi
-            hesapla.hata = 'İkinci sayı sıfır olamaz! Hesaplama yapılamadı'; // sayi2 0 hata degeri yazildi
-            return ; //islemin ilerlemesi durduruldu
+    else if (islem == "division" && sayi2 === 0){
+            hesapla.hata = 'İkinci sayı sıfır olamaz! Hesaplama yapılamadı';
+            return ;
     }
-   if (islem == "bolme") { // girilen isleme gore gerekli matematiksel islemler yapildi ve hesapla Objeckt degiskenine atandi
-   hesapla.sonuc = sayi1 / sayi2 ;
-   } else if(islem == "topla"){
-   hesapla.sonuc = sayi1 + sayi2 ;
-   } else if(islem == "carpma"){
-    hesapla.sonuc = sayi1 * sayi2 ;
-    } else if(islem == "cikarma"){
-    hesapla.sonuc = sayi1 - sayi2 ;
+    switch (islem) {
+      case 'division':
+         hesapla.sonuc = sayi1 / sayi2 ;
+      break;
+      case 'addition':
+         hesapla.sonuc = sayi1 + sayi2 ;
+      break;
+      case 'multiplication':
+         hesapla.sonuc = sayi1 * sayi2 ;
+      break;
+      case 'subtraction':
+         hesapla.sonuc = sayi1 - sayi2 ;
+      break;
+      default:
+        console.log("Böyle işlem yapamayız! Lütfen 50. satırdaki hesap kısmına kısmına division, addition, multiplication,subtraction işlem adı yazınız");
     }
- 
+    
  }
- let hesapla = { // hesapla Objeckt propetiersleri tanimlandi
+ let hesapla = {
     sonuc :"",
     hata : "" 
  };
 
-hesap(32,4,"bolme"); // bolme,topla,carpma,cikarma islemleri girisi buradan yapilir
-console.log(hesapla.sonuc); //sonuc ciktisi Console yazildi
-console.log(hesapla.hata); //hata ciktisi Console yazildi
+hesap(32,4,"division");
+console.log(hesapla.sonuc);
+console.log(hesapla.hata);
