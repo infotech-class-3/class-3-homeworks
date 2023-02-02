937ea5942155ec0bbdb1604ab65972d1e5716f65
Sıla Kızılırmak <silakzlrmk03@gmail.com>
2023-02-02 14:10:05 +0100

    first trial

---
+++ 2023-01-29/17-Sila/hesap.js
@@ -0,0 +1,19 @@
+// Hesap makinesi
+var a;
+var b;
+var x;
+
+a = 10
+b = 5
+x = "+"
+
+    if (x == "+") {
+        console.log(a+b);        
+    } else if (x == "-"){
+        console.log(a-b);
+    } else if (x == "*") {
+        console.log(a*b);        
+    } else if (x == "/") {
+        console.log(a/b);
+    }
+    
