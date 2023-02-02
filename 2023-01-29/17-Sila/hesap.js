77234b68d613163498c73a4556849d9ae935d4e2
Sıla Kızılırmak <silakzlrmk03@gmail.com>
2023-02-03 00:43:32 +0100

    add some changes

--- 2023-01-29/17-Sila/hesap.js
+++ 2023-01-29/17-Sila/hesap.js
@@ -1,4 +1,4 @@
-// Hesap makinesi
+// Hesap makinesi //
 var a;
 var b;
 var x;
@@ -7,46 +7,64 @@ a = 4
 b = 3
 x = "+"
 
+var sonuç;
+// var toplam= {
+//     sonuç: a+b,
+//     not: "işlem başarili"
+//     }
+// var fark= {
+//     sonuç: a-b,
+//     not: "işlem başarili"
+//     }
+// var çarpım= {
+//     sonuç: a*b,
+//     not: "işlem başarili"
+//     }
+// var bölüm= {
+//     sonuç: a/b,
+//     not: "işlem başarili"
+//     }
+// var hata={
+//     sonuç: "bulunamadı",
+//     not: "sayı giriniz"
+// }
+// if (x == "+") {
+//     console.log(toplam)
+//     }
+// else if (x == "-") { 
+//     console.log(fark)
+//     }
+// else if (x == "*") {  
+//     console.log(çarpım)
+//     }
+// else if (x == "/") { 
+//     console.log(bölüm)
+//     }
 
-var toplam= {
-    sonuç: a+b,
-    not: "işlem başarili"
-    }
-var fark= {
-    sonuç: a-b,
-    not: "işlem başarili"
-    }
-var çarpım= {
-    sonuç: a*b,
-    not: "işlem başarili"
-    }
-var bölüm= {
-    sonuç: a/b,
-    not: "işlem başarili"
-    }
-var hata={
-    sonuç: "bulunamadı",
-    not: "sayı giriniz"
-}
-if (x == "+") {
-    console.log(toplam)
-    }
-else if (x == "-") { 
-    console.log(fark)
-    }
-else if (x == "*") {  
-    console.log(çarpım)
-    }
-else if (x == "/") { 
-    console.log(bölüm)
-    }
-
-if (a==Number & b==Number ) {
+// if (a==Number & b==Number ) {
     
-    console.log(toplam)
-}
-else if(a== String||b==String) {
-    console.log(hata);
-}
+//     console.log(toplam)
+// }
+// else if(a== String||b==String) {
+//     console.log(hata);
+// }
 
-
+function calc(a, b, x) {
+    switch (calc) {
+        case toplam: x="+";
+        sonuç= a+b
+            break;
+        case fark:
+            
+            break;
+        case çarpim:
+            
+            break;
+        case bölüm:
+            
+            break;
+        default:
+            break;
+    }
+}
+console.log(sonuç);
