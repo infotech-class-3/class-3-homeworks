3940bb2ec71a4eb10f72cc4edda8ad3991cde67a
Sıla Kızılırmak <silakzlrmk03@gmail.com>
2023-02-02 20:59:19 +0100

    add new changes

--- 2023-01-29/17-Sila/hesap.js
+++ 2023-01-29/17-Sila/hesap.js
@@ -3,17 +3,50 @@ var a;
 var b;
 var x;
 
-a = 10
-b = 5
+a = 4
+b = 3
 x = "+"
 
-    if (x == "+") {
-        console.log(a+b);        
-    } else if (x == "-"){
-        console.log(a-b);
-    } else if (x == "*") {
-        console.log(a*b);        
-    } else if (x == "/") {
-        console.log(a/b);
+
+var toplam= {
+    sonuç: a+b,
+    not: "işlem başarili"
     }
+var fark= {
+    sonuç: a-b,
+    not: "işlem başarili"
+    }
+var çarpım= {
+    sonuç: a*b,
+    not: "işlem başarili"
+    }
+var bölüm= {
+    sonuç: a/b,
+    not: "işlem başarili"
+    }
+var hata={
+    sonuç: "bulunamadı",
+    not: "sayı giriniz"
+}
+if (x == "+") {
+    console.log(toplam)
+    }
+else if (x == "-") { 
+    console.log(fark)
+    }
+else if (x == "*") {  
+    console.log(çarpım)
+    }
+else if (x == "/") { 
+    console.log(bölüm)
+    }
+
+if (a==Number & b==Number ) {
     
+    console.log(toplam)
+}
+else if(a== String||b==String) {
+    console.log(hata);
+}
+
+
