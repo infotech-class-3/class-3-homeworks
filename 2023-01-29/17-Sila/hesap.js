27a31d4d73fdbab1b79613c1bcdd5e784fdc1672
Sıla Kızılırmak <silakzlrmk03@gmail.com>
2023-02-03 16:23:33 +0100

    add changes

--- 2023-01-29/17-Sila/hesap.js
+++ 2023-01-29/17-Sila/hesap.js
@@ -1,37 +1,34 @@
 /*Basic Calculator 
 -> "0/0" ve "sayı/0" için farklı sonuçlar eklendi 
 -> işlem değişkenine 4 işlem sembolleri dışında değer verilmesi durumunda uyarı*/
-
-var sonuç= calc(7, 0, "/");
-console.log(sonuç);
 function calc(a, b, _x) { 
    var sonuç1={
-    sonx: "bulunamadı",
+    işlem_sonucu: "bulunamadı",
     hata: "sayı giriniz"
    };
     switch (_x) {
         case "+":
-            sonuç1.sonx= a+b;
+            sonuç1.işlem_sonucu= a+b;
             sonuç1.hata= "yok"
             break;
         case "-":
-            sonuç1.sonx= a-b;
+            sonuç1.işlem_sonucu= a-b;
             sonuç1.hata= "yok"
             break;
         case "*":
-            sonuç1.sonx= a*b;
+            sonuç1.işlem_sonucu= a*b;
             sonuç1.hata= "yok"
             break;
         case "/":
-            sonuç1.sonx= a/b;
+            sonuç1.işlem_sonucu= a/b;
             sonuç1.hata= "yok"
             switch (b) {
                 case 0:
-                    sonuç1.sonx= "sonsuz";
+                    sonuç1.işlem_sonucu= "sonsuz";
                     sonuç1.hata= "paydaya 0'dan farklı bir değer giriniz" 
                     switch (a) {
                         case 0:
-                            sonuç1.sonx= "tanımsız";
+                            sonuç1.işlem_sonucu= "tanımsız";
                             sonuç1.hata= "0 değeri 0'a bölünmez"
                             break;
                     
@@ -49,7 +46,8 @@ function calc(a, b, _x) {
             sonuç1.hata= "4 işlem sembollerinden birini 3. değişkene tırnak işarei içinde giriniz"
             break;
     }
+    
 return sonuç1
 }
-
-
+var sonuç= calc(7, 1, "/");
+console.log(sonuç);
