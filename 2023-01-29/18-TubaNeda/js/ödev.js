0105bd56c7f22b296ca9ca2d45754a9df3cc5083
TNeda <nedagedikli@outlook.de>
2023-02-06 21:40:29 +0100

    replace console and add return

--- 2023-01-29/18-TubaNeda/js/ödev.js
+++ 2023-01-29/18-TubaNeda/js/ödev.js
@@ -1,45 +1,49 @@
 function hesapla(sayi1,sayi2,islem){
 var deger = {
     sonuc : 0,
-    hata : "Sayi degeri giriniz!"
+    hata1 : "Sayi degeri giriniz!",
+    hata2 : "Sayi2 0 degerini alamaz!"
 };
-if (typeof sayi1 !== "number" ||typeof sayi2 !== "number" ) {
-console.log(deger.hata)        
+if (typeof sayi1 !== "number" ||typeof sayi2 !== "number" ) {    
+return deger.hata1
 }
     
 switch (islem) {
     case "topla":
-        var toplama = sayi1 + sayi2
-        console.log(toplama)
+        deger.sonuc = sayi1 + sayi2
         
         break;
 
     case "cikar":
-        var cikarma = sayi1 - sayi2
-        console.log(cikarma)
+        deger.sonuc = sayi1 - sayi2
             
         break;
 
     case "carp":
-        var carpma = sayi1 * sayi2
-        console.log(carpma)
+        deger.sonuc = sayi1 * sayi2
             
         break;
 
     case "böl":
         if (sayi2 == 0) {
-            error = console.log("2. Sayi 0 degerini alamaz.")
-            return error
+            return deger.hata2
             
         } else {
                 
-        var bölme = sayi1 / sayi2
-        console.log(bölme)
+        deger.sonuc = sayi1 / sayi2
         }    
         break;
                 
     default:
+        deger.sonuc
         break;
 }
+    return deger.sonuc
 }
-hesapla(3,2,"cikar")
+console.log("Toplama islemi sonucu:", hesapla(3,2,"topla"))
+console.log("Cikarma islemi sonucu:", hesapla(3,2,"cikar"))
+console.log("Carpma islemi sonucu:", hesapla(3,2,"carp"))
+console.log("Bölme islemi sonucu:", hesapla(6,2,"böl"))
+
+console.log("Sifira bölme hatasi:", hesapla(6,0,"böl"))
+console.log("Farkli veri tipi hatasi:", hesapla("alti",2,"böl"))
