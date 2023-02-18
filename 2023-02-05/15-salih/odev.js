ba39f8f00a62504615d64c458607219a1de247b6
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-02-18 01:45:16 +0100

    add1

---
+++ 2023-02-05/15-salih/odev.js
@@ -0,0 +1,36 @@
+let str = "adana kozan";
+let str2 = "bursa";
+
+let myFunction = (x) => {
+let check = x[0] === x[x.length-1];
+console.log(check);
+if (check){
+    console.log("string'in ilk ve son karakteri ayni",check);
+}
+else{
+    console.log("string'in ilk ve son karakteri farkli",check);
+}
+}
+
+myFunction(str);
+myFunction(str2);
+myFunction('kars');
+myFunction('ankara');
+
+let myFunction2 = (x) => {
+let arr = x.split("").reverse()
+console.log(arr);
+let reversed = arr.join("");
+
+
+console.log(reversed);
+
+
+
+}
+myFunction2(str);
+
+
+
+
+
