dc470fb63e4918c7c0ce577ff7e902b3db88446f
busrarican <busrarican87@gmail.com>
2023-03-02 16:19:16 +0100

    03-busra promise odev

---
+++ 2023-02-26/odev/03-busra/promise.js
@@ -0,0 +1,32 @@
+function bebekBakimi() {
+        return new Promise((resolve, reject) => {
+          let uyuyor = true;
+          let ac = true;
+          let beziDolu = true;
+      
+        
+          setTimeout(() => {
+            uyuyor = false;
+            console.log("Bebek uyandi.");
+            if (ac && beziDolu) {
+              reject("Bebek ac ve bezi degismeli.");
+            } else if (ac) {
+              reject("Bebek ac.");
+            } else if (beziDolu) {
+              reject("Bebegin bezi degismeli.");
+            } else {
+              resolve("Bebek mutlu.");
+            }
+          }, 5000);
+        });
+      }
+      
+
+      bebekBakimi()
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      
