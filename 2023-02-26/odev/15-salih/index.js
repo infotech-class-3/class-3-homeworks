93a77a1e8331b3b5ee84c5d972de07655056aa0f
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-03-02 23:34:27 +0100

    add1

---
+++ 2023-02-26/odev/15-salih/index.js
@@ -0,0 +1,35 @@
+// function calculate(num1, num2, callback) {
+//     let result = num1 + num2;
+//     callback(result);
+//   }
+  
+//   function displayResult(result) {
+//     console.log("The result is: " + result);
+//   }
+  
+//   calculate(5, 10, displayResult);
+  
+
+const todos= [
+    {title:'todo1'},
+    {title:'todo2'}
+];
+
+function todolist(){
+    setTimeout(() =>{
+        todos.forEach((item)=>{
+            console.log(item);})
+        },2000)
+    }
+
+function newTodo(newTodo, callback){
+    callback();
+    setTimeout(()=>{
+        todos.push(newTodo);
+        
+    },3000);
+}
+
+newTodo(
+    {title:'todo3'}, todolist,
+);
