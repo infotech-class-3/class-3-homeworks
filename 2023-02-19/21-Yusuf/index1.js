a608461ccd01c5ad50693bf3351eac52f2977cb1
Yusuf  Tür <yusufttur@gmail.com>
2023-03-08 16:39:48 +0100

    Update index1.js

--- 2023-02-19/21-Yusuf/index1.js
+++ 2023-02-19/21-Yusuf/index1.js
@@ -1,104 +1,102 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: 'Luke Skywalker',
+    height: 172,
+    mass: 77,
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    name: 'Darth Vader',
+    height: 202,
+    mass: 136,
+    eye_color: 'yellow',
+    gender: 'male',
+  },
+  {
+    name: 'Leia Organa',
+    height: 150,
+    mass: 49,
+    eye_color: 'brown',
+    gender: 'female',
+  },
+  {
+    name: 'Anakin Skywalker',
+    height: 188,
+    mass: 84,
+    eye_color: 'blue',
+    gender: 'male',
+  },
 ];
 
 //***MAP***
 //1. Map all the names
-mappedNames = characters.map((character) =>{ 
-    return {name: character.name}
+mappedNames = characters.map((character) => {
+  return { name: character.name };
 });
 // console.log(mappedNames);
 
 //2. Map all the heights
-mappedHeights = characters.map((character) =>{ 
-    return {height: character.height}
+mappedHeights = characters.map((character) => {
+  return { height: character.height };
 });
 // console.log(mappedHeights);
 
 //3. Map only names and heights
-mappedNamesAndHeights = characters.map((character) =>{ 
-    return {name: character.name, height: character.height}
+mappedNamesAndHeights = characters.map((character) => {
+  return { name: character.name, height: character.height };
 });
 // console.log(mappedNamesAndHeights);
 
 //4. Map only first names
-mappedFirstNames = characters.map((character) =>{ 
-    return {name: character.name.split(' ')[0]}
+mappedFirstNames = characters.map((character) => {
+  return { name: character.name.split(' ')[0] };
 });
 // console.log(mappedFirstNames);
 
 //***REDUCE***
 //1. Total mass of the characters
 reduceMass = characters.reduce((totalMass, character) => {
-    return totalMass + character.mass;
+  return totalMass + character.mass;
 }, 0);
 // console.log(reduceMass);
 
 //2. Total height of the characters
 reduceHeights = characters.reduce((totalHeight, character) => {
-    return totalHeight + character.height;
+  return totalHeight + character.height;
 }, 0);
 // console.log(reduceHeights);
 
 //3. Number of blue eyed characters
 reduceEyeColor = characters.reduce((eyeColor, character) => {
-    eyeColor[character.eye_color] ? eyeColor[character.eye_color]++ : eyeColor[character.eye_color] = 1; 
-    return eyeColor;
+  eyeColor[character.eye_color]
+    ? eyeColor[character.eye_color]++
+    : (eyeColor[character.eye_color] = 1);
+  return eyeColor;
 }, {});
 // console.log(reduceEyeColor);
 
 //4. Total char of the name in the character array
 reduceCharCount = characters.reduce((charCount, character) => {
-    return charCount + character.name.length;
+  return charCount + character.name.length;
 }, 0);
 // console.log(reduceCharCount);
 
 //***FILTER***
 //1. Characters with mass over 100
-filterMassOver100 = characters.filter((character) => 
-    character.mass > 100);
+filterMassOver100 = characters.filter((character) => character.mass > 100);
 // console.log(filterMassOver100);
 
 //2. Characters with mass less than 200
-filterHeightLess200 = characters.filter((character) => 
-    character.height < 200);
+filterHeightLess200 = characters.filter((character) => character.height < 200);
 // console.log(filterHeightLess200);
 
 //3. All male characters
-filterMale = characters.filter((character) => 
-    character.gender == 'male');
+filterMale = characters.filter((character) => character.gender == 'male');
 // console.log(filterMale);
 
 //4. All female characters
-filterFemale = characters.filter((character) => 
-    character.gender == 'female');
+filterFemale = characters.filter((character) => character.gender == 'female');
 // console.log(filterFemale);
 
 //***SORT***
@@ -109,10 +107,30 @@ filterFemale = characters.filter((character) =>
 // console.log(characters.sort((a, b) => a.totalHeight - b.height));
 
 //3. Sort by name
-// console.log(characters.sort((a, b) => a.name - b.name));
+// console.log(
+//   characters.sort((a, b) => {
+//     if (a.name < b.name) {
+//       return -1;
+//     } else if (a.name > b.name) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   })
+// );
 
 //4. Sort by gender
-// console.log(characters.sort((a, b) => a.gender - b.gender));
+// console.log(
+//   characters.sort((a, b) => {
+//     if (a.gender < b.gender) {
+//       return -1;
+//     } else if (a.gender > b.gender) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   })
+// );
 
 //***EVERY***
 //1. Has each character blue eyes?
