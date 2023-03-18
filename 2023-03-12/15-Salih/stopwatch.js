a8ce160f4251453f9617ebcfbf41a3ed532cd81e
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-03-18 22:59:26 +0100

    add1

---
+++ 2023-03-12/15-Salih/stopwatch.js
@@ -0,0 +1,56 @@
+function Kronometre(isim) {
+    this.isim = isim;
+    let baslamaZamani = 0;
+    let bitisZamani = 0;
+    let calisiyormu = false;
+    let gecenSure = 0;
+    
+    this.sifirla = function (){
+     baslamaZamani = 0;
+     bitisZamani = 0;
+     calisiyormu = false;
+     gecenSure = 0;
+    }
+​
+   this.basla = function () {
+        if (calisiyormu) {
+            console.warn('Zaten calisiyor')        
+        } else {
+            baslamaZamani = new Date()
+            calisiyormu = true
+        }
+   }
+​
+   this.dur = function (){
+        if (calisiyormu) {
+            gecenSure = (new Date().getTime() - baslamaZamani.getTime() ) /1000
+            calisiyormu = false
+        } else {
+            console.warn('Zaten calismiyor')
+        }
+   }
+​
+   // defineProperty GET ornegi
+   Object.defineProperty(this, "sonuc", {
+    get: function () { 
+        return gecenSure
+    }
+   })
+​
+   Object.defineProperty(this, "lap", {
+    get: function () { 
+        if (calisiyormu) {
+            return (new Date().getTime() - baslamaZamani.getTime() ) /1000
+        } else {
+            return 0
+        }
+    }
+   }
+   )
+​
+   
+   
+}
+​
+timer1 = new Kronometre('zamanlayici 1')
+timer2 = new Kronometre('zamanlayici 2')
