7fed354948f61a70a1dabf34b930adff10b715ba
mustafadagda <101954112+Mustafadagda@users.noreply.github.com>
2023-03-18 13:02:13 +0100

    12-mustafa

---
+++ 2023-02-26/12-Mustafa/await.js
@@ -0,0 +1,33 @@
+// function sayiTopla(sayilar) {
+//     return new Promise(function(resolve, reject) {
+//       let toplam = 0;
+//       for(let i = 0; i < sayilar.length; i++) {
+//         toplam += sayilar[i];
+//       }
+//       resolve(toplam);
+//     });
+//   }
+  
+//   async function islem() {
+//     let sayilar = [1, 2, 3, 4,5,6,7,8,9];
+//     let toplam = await sayiTopla(sayilar);
+//     console.log("Toplam: " + toplam);
+//   }
+  
+//   islem();
+
+
+function sayiTopla(sayilar) {
+    return new Promise (function(resolve, reject){
+        let toplam = 0;
+        for (let i = 0; i<sayilar.length; i++){
+            toplam += sayilar[i];
+        }
+            resolve (toplam);
+    });
+}async function islem(){
+    let sayilar =[1,2,3,4,5,6,7,8,9];
+    let toplam = await sayiTopla(sayilar);
+    console.log("toplam:" + toplam);
+}
+ islem();
