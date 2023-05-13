c5dc31c733947c86a30a474cef6e7c76974b3d10
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-05-14 01:10:00 +0200

    add1

---
+++ 2023-04-16/15-Salih/users.js
@@ -0,0 +1,26 @@
+const users = [{
+    name: 'ali',
+    age:25,
+    email: 'ali@mail.com'
+},{
+    name: 'ahmet',
+    age:27,
+    email: 'ahdfksj@mail.com'
+},
+{
+    name: 'afdsf',
+    age:15,
+    email: 'alfsafi@mail.com'
+},
+{
+    name: 'dsad',
+    age:35,
+    email: 'alfdsfi@mail.com'
+},
+{
+    name: 'afgdsfli',
+    age:45,
+    email: 'dsadddd@mail.com'
+}];
+
+module.exports = users;
