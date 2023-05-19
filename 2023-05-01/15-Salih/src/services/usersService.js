d230e8fb4009a5c8f61af85db2134b86d9440d0e
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-05-20 00:18:29 +0200

    add1

---
+++ 2023-05-01/15-Salih/src/services/usersService.js
@@ -0,0 +1,101 @@
+const Users = require('../models/Users.js');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+const kayit = async (req, res) => {
+    const user = ({username, name, password, surname} = req.body);
+
+    try{
+        const data = await Users.create(user);
+        return res.json({data: data, message: 'Basarili'});
+    }
+    catch(error){
+        console.log(error);
+        return res.status(400).json({message: "Basarisiz"});
+    }
+}
+
+
+const login = async (req,res) => {
+    const {username, password} = req.body;
+    const user = await Users.findOne({username}).exec();
+
+    if(!user){
+        return res.status(404).json({message: "Kullanici adi bulunmamaktadir!"});
+    }
+
+    const data = {
+        _id : user.id,
+        username: user.username,
+        name: user.name,
+        surname: user.surname,
+        password: user.password
+    }
+
+    // const sifreKontrolu = await user.validatePassword(password);
+
+    // if(!sifreKontrolu){
+    //     return res.status(403).json({message: "Sifreniz yanlis!"});
+    // }
+
+    const token = jwt.sign(data, process.env.Token);
+
+    res.status(200).json({data, token});
+}
+
+const find = async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const user = await Users.findOne({_id: id}).exec();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(404).json({message: 'Kullanici bulunamadi!'});
+    }
+}
+
+const findAll = async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const users = await Users.find().exec();
+        res.status(200).json(users);
+    }
+    catch(err){
+        res.status(404).json({message: 'Kullanicilar bulunamadi!'});
+    }
+}
+
+const update = async (req, res) => {
+    const user = ({id, name, surname, password} = req.body);
+
+    try{
+        const updatedUser = await Users
+        .updateOne({_id: user.id}, {$set: user})
+        .exec();
+        res.status(200).json(updatedUser);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({message: 'Kullanici guncellenemedi!'});
+    }
+}
+
+const remove = async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    try{
+        const deletedUser = await Users
+        .deleteOne({_id: id})
+        .exec();
+        res.status(200).json(deletedUser);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({message: 'Kullanici silinemedi!'});
+    }
+}
+
+module.exports = {kayit, login, find, findAll, update, remove};
