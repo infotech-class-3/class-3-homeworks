f28ce0240cb3dd11fa4ec157941d0b9a6892ae95
salihkaya0 <120326042+salihkaya0@users.noreply.github.com>
2023-05-20 17:43:28 +0200

    add1

---
+++ 2023-05-01/15-salih/calc-odevi/app.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+const express = require("express")
+
+const calc = require("./calc.js")
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
+
+const router = express.Router()
+
+
+router.get('/islem', (req, res) => {
+
+    console.log(req.query)
+    console.log(req.body)
+    const sayi_1 = Number(req.query.sayi_1)
+
+    if (isNaN(sayi_1)) {
+        res.send({sayi_1})
+    }    
+    console.log({sayi_1})
+    const sayi_2 = Number(req.query.sayi_2)
+
+    if (isNaN(sayi_2)) {
+        res.send({sayi_2})
+    }
+
+    console.log({sayi_2})
+    const islem = req.query.islem
+
+
+    const sonuc = calc(sayi_1, sayi_2, islem)
+    res.send(sonuc)
+})
+
+router.post('/islem', (req, res) => {
+
+
+    console.log(req.body)
+    const sayi_1 = parseInt(req.body.sayi_1)
+    const sayi_2 = parseInt(req.body.sayi_2)
+    const islem = req.body.islem
+
+
+    const sonuc = calc(sayi_1, sayi_2, islem)
+    res.send(sonuc)
+    // res.send(req.body)
+})
+
+const port = process.env.PORT
+
+app.use(router)
+
+
+app.listen(port, () => {
+    console.log(`Server basladi: ${port}`)
+})
