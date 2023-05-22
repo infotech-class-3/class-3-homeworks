ccab2ebd6977ccfff748fb05777f46de81d7587a
TNeda <nedagedikli@outlook.de>
2023-05-22 18:00:25 +0200

    add nodejs hw

---
+++ 2023-04-16/18-Neda/odev2/app.js
@@ -0,0 +1,13 @@
+import { ProxyAPI } from "./proxyAPI.js";
+
+const proxy = new ProxyAPI();
+
+console.log(proxy.getValue("ekmek"));  
+console.log(proxy.getValue("ekmek"));  
+console.log(proxy.getValue("ekmek"));  
+console.log(proxy.getValue("ekmek"));  
+console.log(proxy.getValue("su"));  
+console.log(proxy.getValue("su"));  
+console.log(proxy.getValue("ekmek"));  
+console.log(proxy.getValue("ekmek"));  
+   
---
+++ 2023-04-16/18-Neda/odev2/urunListesi.js
@@ -0,0 +1,6 @@
+export default [
+    {type: 'ekmek', price:'0.50 €'},
+    {type: 'su', price:'0.25 €'},
+    {type: 'elma', price:'1.50 €'},
+    {type: 'patates', price:'1.75 €'},
+]
