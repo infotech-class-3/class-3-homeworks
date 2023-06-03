b6e0f5c79b64fd441b91e0cdc29f325660f9382f
MehmetAlp <alpins1071@gmail.com>
2023-06-03 13:02:12 +0200

    todojs e devam

---
+++ 2023-05-07/10-MehmetAlp/src/models/Todo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true   },
+
+    todo : {
+        type : String,
+        required : true
+    },
+
+    completed : {
+        type : Boolean,
+        default : false
+    },
+
+    createDate : {
+        type : Date,
+        default : Date.now()
+    }
+
+    updatedDate : {
+        type : Date,
+        default : Date.now()
+    }
+});
+
+const Todo = mongoose.model("Todos", todoSchema);
+
+module.exports = Todo;
