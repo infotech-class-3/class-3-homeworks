2a8105e57f0463eabf0e47a71496d338b8a8e32b
mahocan1 <120319021+mahocan1@users.noreply.github.com>
2023-06-03 22:45:19 +0200

    vue

---
+++ 2023-05-28/11-Muhammet/my/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    HELLO {{ name }} {{ surname }} - {{ age }}
+    <br/>
+    <div> {{ fullName }} </div>
+    <br/>
+    <div>
+      <button v-on:click="arttır" >Arttır</button>
+    <br/>
+      <button v-on:click="azalt">Azalt</button>
+    </div>
+   
+    <div> {{ counter }} </div>
+  </div>
+  
+  
+ 
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+      name: "Mahmut",
+      surname: "yılmaz",
+      age: 41,
+      course: ["vue", "react", "angelar"],
+      counter:0,
+    };
+    
+  },
+ computed: {
+    fullName() {
+      return  `${this.name} ${this.surname} --  ${this.age} `;
+    }
+  },
+  methods: {
+    arttır() {
+      this.counter++;
+
+    },
+    azalt() {
+      this.counter--;
+
+    },
+  },
+};
+
+</script>
+
+<style>
+
+</style>
+
+
