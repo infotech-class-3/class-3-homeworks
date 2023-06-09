61eb3e9d0e0db687399133efccc63d18c0d0748b
Furkan Kilicer <furkankilicer@gmail.com>
2023-06-09 10:10:31 +0200

    Vue Component, Routes... Exercises

---
+++ 2023-06-06/05-Furkan/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <h1>HOME SAYFAS</h1>
+    <v-btn v-on:click="onClick('/users')" class="mr-5">Kullanicilar</v-btn>
+    <v-btn v-on:click="onClick('/about')">Hakkimizda</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+
+  methods: {
+    onClick(path) {
+      this.$router.push(path);
+    },
+  },
+};
+</script>
---
+++ 2023-06-06/05-Furkan/src/views/UsersView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>BURASI USERS SAYFASIDIR...</h1>
+
+    <UserCard
+      v-for="user in users"
+      :key="user.id"
+      class="mb-5"
+      :user-data="user"
+    />
+  </div>
+</template>
+
+<script>
+import UserCard from "../components/UserCard.vue";
+
+export default {
+  name: "UsersView",
+  components: {
+    UserCard,
+  },
+
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Osman", age: "21", complited: true },
+        { id: 2, name: "Ayse", age: "30", complited: false },
+        { id: 3, name: "Ahmet", age: "18", complited: true },
+        { id: 4, name: "Zeynep", age: "40", complited: false },
+      ],
+    };
+  },
+};
+</script>
