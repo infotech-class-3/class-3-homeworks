0653a99ab9e022b5a2e81bec6ee782f2d3ea9a49
MehmetAlp <alpins1071@gmail.com>
2023-06-11 09:34:32 +0200

    wieder

---
+++ 2023-05-28/10-MehmetAlp/my-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>{{ name }} {{ surName }}--{{ age }} </div>
+    <div>{{ courses[0] }} </div>
+    <div>{{ user.userName }} {{ user.userPassword }} </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+   return {
+    name:"mehmet",
+    surName: "yilmaz",
+    age: 55,
+    courses: ["nodejs","css","js","vuejs"],
+    user: {
+      userName:"Memo",
+      userPassword:"1234",
+      confirmPassword:""
+    }
+   } 
+  }
+}
+</script>
+
+<style>
+
+</style>
