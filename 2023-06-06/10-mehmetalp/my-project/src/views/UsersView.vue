66f4b3c897fec06a4ff3557a08c0a4d0c1049a19
MehmetAlp <alpins1071@gmail.com>
2023-06-14 00:09:51 +0200

    Update UsersView.vue

--- 2023-06-06/10-mehmetalp/my-project/src/views/UsersView.vue
+++ 2023-06-06/10-mehmetalp/my-project/src/views/UsersView.vue
@@ -4,19 +4,26 @@
             <h1>Kullanicilar sayfasi</h1>
         </div>
 
-        <UserCard></UserCard>
-        <UserCard></UserCard>
-        <UserCard></UserCard>
+        <UserCard v-for="user in users" :key="user.id" :user-data="user" :isActive="user.isActive"/>
+        
     </div>
 </template>
 
 <script>
-import UserCard from "../components/UserCard.vue";
+import UserCard from "../components/UserCard.vue"
 
 export default {
     name: "UsersView",
     componenets: {
         UserCard,
     },
+    data(){
+       return {users:[
+            {id:1, adi:"Osman", yasi:21, isActive:false},
+            {id:2, adi:"Ali", yasi:33, isActive:true},
+            {id:3, adi:"Mehmet", yasi:67, isActive:false}
+
+        ]}
+    }
 };
 </script>
